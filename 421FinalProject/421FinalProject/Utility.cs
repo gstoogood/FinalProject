ec0e2e8a22f9400127dc88d37e4dc196ffe8c507
Graham Toogood <gstoogood@crimson.ua.edu>
2020-12-02 12:09:08 -0600

    test

---
+++ 421FinalProject/421FinalProject/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _421FinalProject
+{
+    public static class Utility
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
