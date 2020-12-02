4c6198062593f92df678f7e4c15976e24482ae62
RussellGoodloe <50248231+RussellGoodloe@users.noreply.github.com>
2020-12-02 13:02:44 -0600

    added 2 of the data classes

---
+++ 421FinalProject/421FinalProject/Models/Destination.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _421FinalProject.Models
+{
+    public class Destination
+    {
+        [Key]
+        public int DestID { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string OfficialLanguage { get; set; }
+
+        [Required]
+        public byte[] Image1 { get; set; }
+
+        public byte[] Image2 { get; set; }
+    }
+}
---
+++ 421FinalProject/421FinalProject/Models/Location.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _421FinalProject.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocID { get; set; }
+
+        [Required]
+        [ForeignKey("Destination")]
+        public string DestID { get; set; }
+
+        [Required]
+        public string Subset { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+
+        public string Note { get; set; }
+
+        [Required]
+        public string PriceRange { get; set; }
+
+        [Required]
+        public byte[] Image1 { get; set; }
+
+        public byte[] Image2 { get; set; }
+
+        public virtual Destination Destination { get; set; }
+
+    }
+}
