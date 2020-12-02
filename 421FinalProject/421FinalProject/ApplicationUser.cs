10b119947043054c34393e3bfda75876caa35430
Graham Toogood <gstoogood@crimson.ua.edu>
2020-12-02 13:35:26 -0600

    applicationuser

---
+++ 421FinalProject/421FinalProject/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _421FinalProject
+{
+    public class ApplicationUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
